d5cc672fe7d8fc7c1995bd96e1e1dadfdf8d4e90
Nattan Aguia <nattan23@msn.com>
2025-06-12 19:14:52 -0400

    create login and logout routes

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -29,3 +29,37 @@ export const signUp = async (req, res) => {
     res.status(500).json(message.error);
   }
 };
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return res.status(401).json({ message: "User not found" });
+
+    const validPassword = await bcryptjs.compare(password, validUser.password);
+    if (!validPassword)
+      return res.status(401).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET_KEY, {
+      expiresIn: "1d",
+    });
+
+    res.cookie("token", token, { httpOnly: true });
+
+    res.status(200).json({
+      id: validUser._id,
+      username: validUser.username,
+      email: validUser.email,
+      createdAt: validUser.createdAt,
+      updatedAt: validUser.updatedAt,
+    });
+  } catch (error) {
+    res.status(500).json(message.error);
+  }
+};
+
+export const logout = async (req, res) => {
+    res.clearCookie("token");
+    res.status(200).json({ message: "Logged out successfully" });
+  };
--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./config/connection.js";
+import authRoutes from "./routes/auth.routes.js"
 
 dotenv.config();
 
@@ -13,6 +14,9 @@ connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
+
+
+app.use("/api", authRoutes)
 
 app.listen(PORT, () => {
   console.log("Server listening on PORT", PORT);
